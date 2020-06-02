68eb471b3c53b94936e4df68a3f6a478bf43470b
Gigas Taufan <gigastaufan@gmail.com>
2020-06-02 08:05:14 +0700

    deleting unnecessary

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -6,14 +6,6 @@ const main = () => {
     const searchBar = document.querySelector("search-box");
     const mealList = document.querySelector("meal-list");
 
-    // const onButtonSearchClicked = () => {
-    //     // const dataSource = new DataSource(renderResult, fallbackResult);
-    //     // dataSource.searchMeal(searchBar.value);
-    //     DataSource.searchMeal(searchBar.value)
-    //         .then(renderResult)
-    //         .catch(fallbackResult)
-    // };
-
     const onButtonSearchClicked = async () => {
         try {
             const result = await DataSource.searchMeal(searchBar.value);
@@ -24,36 +16,13 @@ const main = () => {
     };
 
     const renderResult = results => {
-        // mealList.innerHTML = "";
-        // results.forEach(function (meal) {
-        //     const {
-        //         name,
-        //         fanArt,
-        //         country,
-        //         instruction
-        //     } = meal;
-
-        //     const mealElement = document.createElement("div");
-        //     mealElement.setAttribute("class", "meal");
-
-        //     mealElement.innerHTML = '<img class="fan-art-meal" src="' + fanArt + '" alt="Fan Art">\n' +
-        //         '<div class="meal-info">\n' +
-        //         '<h2>' + name + '</h2>\n' +
-        //         '<h4>' + country + '</h4>' +
-        //         '<p>' + instruction + '</p>' +
-        //         '</div>';
-        //     mealList.appendChild(mealElement);
-        // })
         mealList.meals = results;
     };
 
     const fallbackResult = message => {
-        // mealList.innerHTML = "";
-        // mealList.innerHTML += '<h2 class="placeholder">' + message + '</h2>'
         mealList.renderError(message);
     };
 
-    // searchBtn.addEventListener("click", onButtonSearchClicked);
     searchBar.clickEvent = onButtonSearchClicked;
 };
 
